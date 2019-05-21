63f55ac2ec07a658bbb7cc61cd07f88c3dcf4705
ISSILAHI <ISSILAHI@163.173.118.248>
2019-05-21 16:58:46 +0200

    commit Article et auteur achevés

--- Utopia/src/main/java/org/utopia/internationale/controller/ArticleController.java
+++ Utopia/src/main/java/org/utopia/internationale/controller/ArticleController.java
@@ -1,5 +1,119 @@
 package org.utopia.internationale.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.utopia.internationale.dao.ArticleRepository;
+import org.utopia.internationale.dao.AuteurRepository;
+import org.utopia.internationale.entities.Article;
+import org.utopia.internationale.entities.Auteur;
+
+@Controller
 public class ArticleController {
 
+	@Autowired
+	private ArticleRepository articleRepository;
+	@Autowired
+	private AuteurRepository auteurRepository;
+	
+	@Value("${dir.images}")
+	private String imagesDir;
+	
+	@RequestMapping(value="/articles")
+	public String consulter(Model model) {
+		List<Article> listArticles = articleRepository.findAll();
+		model.addAttribute("listArticles", listArticles);
+		return "article/articles";
+	}
+	
+	@RequestMapping(value="/formArticle",method=RequestMethod.GET)
+	public String formAuteur(Model model) {
+		model.addAttribute("listAuteurs",auteurRepository.findAll());
+		model.addAttribute("article", new Article());
+		return "article/FormArticle";
+	}
+	
+	/*@RequestMapping(value="/SaveArticle",method=RequestMethod.POST)
+	public String save(Model model,@Valid Article article, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()){
+			return "article/FormArticle";
+		}
+		articleRepository.save(article);
+		model.addAttribute("article",article);
+		return "redirect:/articles";
+	}*/
+	
+	@RequestMapping(value="/SaveArticle",method=RequestMethod.POST)
+	public String save(Model model,@Valid Article article, 
+			BindingResult bindingResult,
+			@RequestParam(name="picture")MultipartFile file) throws Exception {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("listAuteurs", auteurRepository.findAll());
+			return "article/FormArticle";
+		}
+		if(!(file.isEmpty())){
+			article.setImage(file.getOriginalFilename());
+		}
+		articleRepository.save(article);
+		if(!(file.isEmpty())){
+			article.setImage(file.getOriginalFilename());
+			//file.transferTo(new File(System.getProperty("user.home")+"/sco/"+file.getOriginalFilename()));
+			//file.transferTo(new File(imagesDir+file.getOriginalFilename()));
+			//Correcte
+			file.transferTo(new File(imagesDir+article.getIdArticle()));
+		}
+		
+		return "redirect:/articles";
+	}
+	
+	@RequestMapping(value="/editArticle",method=RequestMethod.GET)
+	public String edit(Model model, Long id) {
+		Article article = articleRepository.findOne(id);
+		model.addAttribute("article", article);
+		model.addAttribute("listAuteurs",auteurRepository.findAll());
+		return "article/EditArticle";
+	}
+	
+	@RequestMapping(value="/UpdateArticle",method=RequestMethod.POST)
+	public String update(@Valid Article article, 
+			BindingResult bindingResult){
+		if(bindingResult.hasErrors()) {
+			return "EditArticle";
+		}
+		articleRepository.save(article);
+		return "redirect:/articles";
+	}
+	
+	@RequestMapping(value="/supprimerArticle",method=RequestMethod.GET)
+	public String supprimer(Long id) {
+		Article article = articleRepository.findOne(id);
+		articleRepository.delete(article);
+		return "redirect:/articles";
+	}
+	
+	@RequestMapping(value="/getPhoto", produces=MediaType.IMAGE_JPEG_VALUE)
+	@ResponseBody
+	public byte[] getPhoto(Long id) throws Exception{
+		File f = new File(imagesDir+id);
+		return IOUtils.toByteArray(new FileInputStream(f));
+	}
+	
+	
+	
 }
--- Utopia/src/main/java/org/utopia/internationale/controller/AuteurController.java
+++ Utopia/src/main/java/org/utopia/internationale/controller/AuteurController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.utopia.internationale.dao.AuteurRepository;
 import org.utopia.internationale.entities.Auteur;
 
@@ -21,8 +22,13 @@ public class AuteurController {
 	@Autowired
 	private AuteurRepository auteurRepository;
 	
-	@RequestMapping(value="/auteurs")
-	public String consulter(Model model,@RequestParam(name="page",defaultValue="0")int page,
+	@RequestMapping(value="/index")
+	public String pageIndex() {
+		return "index";
+	}
+	
+	/*@RequestMapping(value="/auteurs")
+	public String index(Model model,@RequestParam(name="page",defaultValue="0")int page,
 			@RequestParam(name="size",defaultValue="5")int size,
 			@RequestParam(name="motCle",defaultValue="")String mc) {
 		//List<Auteur> listAuteurs = auteurRepository.findAll();
@@ -36,24 +42,45 @@ public class AuteurController {
 		model.addAttribute("pageCourante", page);
 		model.addAttribute("pages", pages);
 		model.addAttribute("listAuteurs", listAuteurs);
-		return "auteurs";
+		return "tables";
+	}*/
+	
+	@RequestMapping(value="/auteurs")
+	public String consulter(Model model) {
+		List<Auteur> listAuteurs = auteurRepository.findAll();
+		//Page<Auteur> listAuteurs = auteurRepository.findAll(new PageRequest(p, size));
+		/*Page<Auteur> listAuteurs = auteurRepository.chercher("%"+mc+"%",new PageRequest(page, size));
+		int pagesCount = listAuteurs.getTotalPages();
+		int[] pages = new int[pagesCount];
+		for(int i=0;i<pagesCount;i++) pages[i]=i;
+		model.addAttribute("motCle", mc);
+		model.addAttribute("size", size);
+		model.addAttribute("pageCourante", page);
+		model.addAttribute("pages", pages);
+		model.addAttribute("listAuteurs", listAuteurs);*/
+		model.addAttribute("listAuteurs", listAuteurs);
+		return "auteur/auteurs";
 	}
 	
 	@RequestMapping(value="/formAuteur",method=RequestMethod.GET)
 	public String formAuteur(Model model) {
 		model.addAttribute("auteur", new Auteur());
-		return "FormAuteur";
+		return "auteur/FormAuteur";
 	}
 	
 	@RequestMapping(value="/SaveAuteur",method=RequestMethod.POST)
-	public String save(Model model,@Valid Auteur auteur, BindingResult bindingResult) {
+	public String save(Model model,@Valid Auteur auteur, BindingResult bindingResult,
+			@RequestParam(name="photo")MultipartFile file) {
 		
 		if(bindingResult.hasErrors()){
-			return "FormAuteur";
+			return "auteur/FormAuteur";
 		}
+		
+		
+		
 		auteurRepository.save(auteur);
 		model.addAttribute("auteur",auteur);
-		return "ConfirmationAuteur";
+		return "redirect:/auteurs";
 	}
 	
 	@RequestMapping(value="/editAuteur",method=RequestMethod.GET)
@@ -80,4 +107,5 @@ public class AuteurController {
 		auteurRepository.delete(auteur);
 		return "redirect:/auteurs?page="+page+"&size="+size+"&motCle="+motCle;
 	}
+	
 }
